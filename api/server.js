0a594291ec0ed0eeddf351154e58c77ab5e3d062
Jáder Oliveira <jaderoliveiraa@zapmais.net>
2026-01-17 23:29:37 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import routes from "./src/routes.js";
 const app = express();
-const port = process.env.PORT||3000;
+const port = process.env.PORT||3005;
 app.use(express.json());
 app.get("/", (_,res)=>res.json({ok:true}));
 app.use("/api", routes);
